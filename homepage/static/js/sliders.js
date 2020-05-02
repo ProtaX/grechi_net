2d7139f64a7209802dad9fb67f3f55a73852aade
Arseniy Karpenko <karpenko1298@gmail.com>
2020-05-02 20:52:35 +0300

    chrome gui mostly done

--- homepage/static/js/sliders.js
+++ homepage/static/js/sliders.js
@@ -23,24 +23,33 @@ var wb_per_meal_out_days = document.getElementById("wb-per-meal-days")
 
 var dney = document.getElementById("dney")
 var paketov = document.getElementById("paketov")
+var nujno = document.getElementById("nujno")
 
 var wb_packages_enough = document.getElementById("wb-packages-enough")
 var days_enough = document.getElementById("days-enough")
 
 function calculateDaysEnough() {
-    updateDaysEnough();
-    var meals_per_day = parseInt(meals_per_day_out_wb.innerText);
-    var package_value = parseInt(package_volume_out_wb.innerText);
-    var hungry_people = parseInt(hungry_people_out_wb.innerText);
+    var meals_per_day = parseInt(meals_per_day_out_days.innerText);
+    var package_value = parseInt(package_volume_out_days.innerText);
+    var hungry_people = parseInt(hungry_people_out_days.innerText);
     var packages_count = parseInt(packages_count_out.innerText);
-    var wb_per_meal = parseInt(wb_per_meal_out_wb.innerText);
+    var wb_per_meal = parseInt(wb_per_meal_out_days.innerText);
 
-    var res = Math.round(packages_count * package_value / meals_per_day / hungry_people / wb_per_meal);
+    var res = Math.round((packages_count * package_value) / (meals_per_day * hungry_people * wb_per_meal));
     days_enough.innerHTML = res;
+    updateDaysEnough();
 }
 
 function calculateWbEnough() {
+    var meals_per_day = parseInt(meals_per_day_out_wb.innerText);
+    var package_value = parseInt(package_volume_out_wb.innerText);
+    var hungry_people = parseInt(hungry_people_out_wb.innerText);
+    var days_count = parseInt(days_out.innerText);
+    var wb_per_meal = parseInt(wb_per_meal_out_wb.innerText);
 
+    var res = Math.round(days_count * meals_per_day * wb_per_meal * hungry_people / package_value);
+    wb_packages_enough.innerHTML = res;
+    updateWbEnough();
 }
 
 function updateDaysEnough() {
@@ -65,65 +74,74 @@ function updateDaysEnough() {
 }
 
 function updateWbEnough() {
+    var full_val = parseInt(wb_packages_enough.innerText);
     var val = parseInt(wb_packages_enough.innerText) % 10
-    if (val == 1) {
-        paketov.innerHTML = "пакет";
-    } else if (val > 1 && val < 5) {
+    if (full_val >= 10 && full_val <= 20) {
+        paketov.innerHTML = "пакетов";
+        nujno.innerHTML = "Нужно";
+        return;
+    }
+    if (val >= 2 && val <= 4) {
         paketov.innerHTML = "пакета";
+        nujno.innerHTML = "Нужно";
+    } else if (val == 1) {
+        paketov.innerHTML = "пакет";
+        nujno.innerHTML = "Нужен";
     } else {
         paketov.innerHTML = "пакетов";
+        nujno.innerHTML = "Нужно";
     }
 }
 
 
 // Days enough sliders
 packages_count_slider.oninput = function() {
-    calculateDaysEnough();
     packages_count_out.innerHTML = this.value;
+    calculateDaysEnough();
 }
 
 meals_per_day_slider_days.oninput = function() {
-    calculateDaysEnough();
     meals_per_day_out_days.innerHTML = this.value;
+    calculateDaysEnough();
 }
 
 hungry_people_slider_days.oninput = function() {
-    calculateDaysEnough();
     hungry_people_out_days.innerHTML = this.value;
+    calculateDaysEnough();
 }
 
 package_volume_slider_days.oninput = function() {
-    calculateDaysEnough();
     package_volume_out_days.innerHTML = this.value;
+    calculateDaysEnough();
 }
 
 wb_per_meal_slider_days.oninput = function() {
-    calculateDaysEnough();
     wb_per_meal_out_days.innerHTML = this.value
+    calculateDaysEnough();
 }
 
 // Wb enough sliders
 meals_per_day_slider_wb.oninput = function() {
-    updateWbEnough();
     meals_per_day_out_wb.innerHTML = this.value;
+    calculateWbEnough();
 }
 
 hungry_people_slider_wb.oninput = function() {
-    updateWbEnough();
     hungry_people_out_wb.innerHTML = this.value;
+    calculateWbEnough();
 }
 
 package_volume_slider_wb.oninput = function() {
-    updateWbEnough();
     package_volume_out_wb.innerHTML = this.value;
+    calculateWbEnough();
 }
 
 days_slider.oninput = function() {
-    updateWbEnough();
     days_out.innerHTML = this.value;
+    calculateWbEnough();
 }
 
 wb_per_meal_slider_wb.oninput = function() {
-    updateWbEnough();
     wb_per_meal_out_wb.innerHTML = this.value
+    calculateWbEnough();
 }
